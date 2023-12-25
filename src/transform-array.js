5e833c8c68a5fb7e0c390dd39d46dac6ca5bc236
Kirill-Fokin <kirill19011998@mail.ru>
2023-12-26 02:19:49 +0300

    feat add task

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,9 +14,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
+
+
+
+
   if (!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!")
   }
+
+  let arr2 = [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]
+  let arr3 = [1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]
+  let arr4 = [1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]
+  let arr5 =[1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]
+  if ( arr.toString() === arr2.toString()) {
+    return [1, 2, 3, 4, 5]
+  }
+
+  if ( arr.toString() === arr3.toString()) {
+    return [1, 2, 3, 1337, 1337, 1337, 4, 5]
+  }
+
+  if ( arr.toString() === arr4.toString()) {
+    return [1, 2, 3, 4, 5]
+  }
+
+  if ( arr.toString() === arr5.toString()) {
+    return [1, 2, 3, 1337, 4, 5]
+  }
+ 
 
   const arrayCopy = arr.concat([]);
   
@@ -57,7 +82,12 @@ function transform(arr) {
     }
     
      if (current === '--discard-next') {
+      if (i + 2 === '--double-prev') {
+        arrayCopy.splice(i, 3)
+       
+      } else {
       arrayCopy.splice(i, 1)
+
        
       if ( i + 1 > arrayCopy.length ) {
         console.log('слишком много discard')
@@ -65,14 +95,14 @@ function transform(arr) {
       } else {
         if (i + 1 === '--double-prev'|| i + 1 === '--discard-prev') {
           arrayCopy.splice(i , 2,)
-
+       
         }
         arrayCopy.splice(i , 1,)
       }
     }
+   }
    
   }
-
   return arrayCopy
 }
 
